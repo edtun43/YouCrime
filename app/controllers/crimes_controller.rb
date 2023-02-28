501763038293696e0784ac305bd1329b1ec6cea6
Edin Tuna <ed.tun@outlook.com>
2023-02-28 13:09:04 +0100

    added actions + _form partial

--- app/controllers/crimes_controller.rb
+++ app/controllers/crimes_controller.rb
@@ -1,4 +1,6 @@
 class CrimesController < ApplicationController
+  before_action :set_crime, only: %i[edit update destroy]
+
   def index
     @crimes = Crime.all
   end
@@ -18,9 +20,25 @@ class CrimesController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    @crime.update(crime_params)
+  end
+
+  def destroy
+    @crime.destroy
+    redirect_to crimes_path
+  end
+
   private
 
   def crime_params
     params.require(:crime).permit(:category, :description, :price)
+  end
+
+  def set_crime
+    @crime = Crime.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :crimes, except: %i[destroy update edit] do
+  resources :crimes do
     resources :bookings, only: %i[new create]
   end
   resources :bookings, only: [:index] # index is our dashboard

157a4001d25c52cf0f98b53108deeac87ac9f5d1
Julian Schopen <j-schopen@web.de>
2023-03-01 12:18:48 +0100

    updated seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,10 +9,50 @@
 Crime.destroy_all
 User.destroy_all
 
-frank = User.create!(email: "frank@gmail.com", password: "password")
-edin = User.create!(email: "edin@gmail.com", password: "password")
+frank = User.create!(email: "frank@gardener.com", password: "password", username: "Zodiak")
+edin = User.create!(email: "edin@gmail.com", password: "password", username: "The Swift")
+jeremy = User.create!(email: "jeremy@no-suspect.com", password: "password", username: "Unseen")
+julia = User.create!(email: "julia@nocriminal.com", password: "password", username: "Eve")
 
-Crime.create!(category: "Murder", description: "killing someone", price: 180, user: frank)
-Crime.create!(category: "Sabotage", description: "sabotaging a local store", price: 69, user: edin)
+Crime.create!(category: "Murder", name: "Professional and silent", bio: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+  molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+  numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+  optio, eaque rerum! Provident similique accusantium nemo autem.", progress: 3, description: "Veritatis
+  obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
+  nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
+  tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
+  quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos
+  sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam
+  recusandae alias error harum maxime adipisci amet laborum.", price: 10.000, user: frank)
+Crime.create!(category: "Sabotage", name: "subtile changes that change the world", bio: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+  molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+  numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+  optio, eaque rerum! Provident similique accusantium nemo autem.", progress: 9, description: "Veritatis
+  obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
+  nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
+  tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
+  quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos
+  sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam
+  recusandae alias error harum maxime adipisci amet laborum.", price: 3.000, user: edin)
+Crime.create!(category: "Theft", name: "They won't know", bio: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+  molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+  numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+  optio, eaque rerum! Provident similique accusantium nemo autem.", progress: 4, description: "Veritatis
+  obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
+  nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
+  tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
+  quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos
+  sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam
+  recusandae alias error harum maxime adipisci amet laborum.", price: 600, user: jeremy)
+Crime.create!(category: "Assault", name: "sweet surprise", bio: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+  molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+  numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+  optio, eaque rerum! Provident similique accusantium nemo autem.", progress: 7, description: "Veritatis
+  obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
+  nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
+  tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
+  quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos
+  sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam
+  recusandae alias error harum maxime adipisci amet laborum.", price: 150, user: julia)
 
 puts "created seeds"

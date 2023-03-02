33430297413ea53cc778e4cb2fe2ffcf44ff9c1d
Jeffrey Boateng <jeff05@gmx.net>
2023-03-02 14:24:45 +0100

    updated index page

--- app/models/crime.rb
+++ app/models/crime.rb
@@ -4,6 +4,6 @@ class Crime < ApplicationRecord
   validates :category, presence: true, inclusion: { in: %w[Murder Theft Sabotage Assault Kidnap Forgery] }
   validates :description, presence: true
   validates :name, presence: true
-  validates :bio, presence: true
+  validates :bio, presence: true, length: { minimum: 60, maximum: 100 }
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 1 }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -14,40 +14,28 @@ edin = User.create!(email: "edin@gmail.com", password: "password", username: "Th
 jeremy = User.create!(email: "jeremy@no-suspect.com", password: "password", username: "Unseen")
 julia = User.create!(email: "julia@nocriminal.com", password: "password", username: "Eve")
 
-Crime.create!(category: "Murder", name: "Professional and silent", bio: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
-  molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
-  numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
-  optio, eaque rerum! Provident similique accusantium nemo autem.", progress: 3, description: "Veritatis
+Crime.create!(category: "Murder", name: "Professional and silent", bio: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet", progress: 3, description: "Veritatis
   obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
   nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
   tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
   quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos
   sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam
   recusandae alias error harum maxime adipisci amet laborum.", price: 10000, user: frank)
-Crime.create!(category: "Sabotage", name: "subtile changes that change the world", bio: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
-  molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
-  numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
-  optio, eaque rerum! Provident similique accusantium nemo autem.", progress: 9, description: "Veritatis
+Crime.create!(category: "Sabotage", name: "subtile changes that change the world", bio: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet", progress: 9, description: "Veritatis
   obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
   nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
   tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
   quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos
   sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam
   recusandae alias error harum maxime adipisci amet laborum.", price: 3000, user: edin)
-Crime.create!(category: "Theft", name: "They won't know", bio: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
-  molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
-  numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
-  optio, eaque rerum! Provident similique accusantium nemo autem.", progress: 4, description: "Veritatis
+Crime.create!(category: "Theft", name: "They won't know", bio: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet", progress: 4, description: "Veritatis
   obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
   nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
   tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
   quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos
   sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam
   recusandae alias error harum maxime adipisci amet laborum.", price: 600, user: jeremy)
-Crime.create!(category: "Assault", name: "sweet surprise", bio: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
-  molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
-  numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
-  optio, eaque rerum! Provident similique accusantium nemo autem.", progress: 7, description: "Veritatis
+Crime.create!(category: "Assault", name: "sweet surprise", bio: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet", progress: 7, description: "Veritatis
   obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
   nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
   tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,

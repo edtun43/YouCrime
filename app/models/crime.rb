0a1ec600e53cea6d09e2ad252aee228499d3ea28
Jeffrey Boateng <jeff05@gmx.net>
2023-03-02 16:03:00 +0100

    updates done styling errors fixex

--- app/models/crime.rb
+++ app/models/crime.rb
@@ -4,6 +4,6 @@ class Crime < ApplicationRecord
   validates :category, presence: true, inclusion: { in: %w[Murder Theft Sabotage Assault Kidnap Forgery] }
   validates :description, presence: true
   validates :name, presence: true
-  validates :bio, presence: true, length: { minimum: 60, maximum: 100 }
+  # validates :bio, presence: true, length: { minimum: 100, maximum: 200 }
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 1 }
 end

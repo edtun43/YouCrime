2d46bdacda38dbbbb45562b5e492a6c73b51bba0
Edin Tuna <ed.tun@outlook.com>
2023-03-03 15:00:51 +0100

    calenderfinished

--- app/javascript/controllers/booking_date_controller.js
+++ app/javascript/controllers/booking_date_controller.js
@@ -3,19 +3,23 @@ import flatpickr from "flatpickr";
 
 // Connects to data-controller="booking-date"
 export default class extends Controller {
+  static targets = ["input"]
   static values = { booked: Array }
 
   connect() {
+    console.log(this.bookedValue);
+    console.log("hello");
     this.config = {
       disable: this.bookedValue,
       minDate: "today"
     }
-    flatpickr(this.element, this.config);
+    flatpickr(this.inputTarget, this.config);
   }
 
-  blacklist(event) {
-    event.preventDefault();
-    this.config.disable = [`${this.element.value}`];
-    flatpickr(this.element, this.config);
-  }
+  // blacklist(event) {
+  //   event.preventDefault();
+  //   this.config.disable = [`${this.element.value}`];
+  //   flatpickr(this.element, this.config);
+  //   console.log(this.element.value);
+  // }
 }

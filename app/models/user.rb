a607e73537a67273ae30fd534aa4e6f8b158495b
Edin Tuna <ed.tun@outlook.com>
2023-03-03 14:15:46 +0100

    Calender-Logic

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,10 @@ class User < ApplicationRecord
   has_many :crimes, dependent: :destroy
   # validates :username, uniqueness: true, presence: true
   # validates :username, length: { minimum: 4 }
+
+  def booked_days
+    crimes.map do |crime|
+      crime.bookings.map { |booking| booking.date.strftime("%Y-%m-%d") }
+    end.flatten
+  end
 end
